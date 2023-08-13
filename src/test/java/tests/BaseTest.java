64ef5faff906eaa05fbe2f1285ce88cd82c44770
Mikhailqwqwqwqwqwqwqwqwqwqwqwqwq <mishavitebsk96@gmail.com>
2023-08-13 22:57:08 +0300

    Update

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -6,12 +6,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import pages.LoginPage;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
 
     WebDriver driver;
+    LoginPage loginPage;
 
 
 
@@ -24,6 +26,8 @@ public class BaseTest {
         options.addArguments("--incognito");
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        loginPage = new LoginPage(driver);
 
 
 
