6b2942733b89cd011543c12b68e2cf9c8f5ba501
Mikhailqwqwqwqwqwqwqwqwqwqwqwqwq <mishavitebsk96@gmail.com>
2023-08-14 12:07:29 +0300

    Update

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+public class HomePage extends BasePage {
+
+public static final By LOGOUT_LINK = By.xpath("//button[@class=\"user-menu__btn\" and @ng-click=\"logout($event)\"]");
+
+
+
+}
--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -7,19 +7,21 @@ import static org.bouncycastle.cms.RecipientId.password;
 
 public class LoginPage extends BasePage {
 
-    public static final By LOGIN_IN_BUTTON = By.xpath("//button[@type='submit']");
+    public static final By LOG_IN_BUTTON = By.xpath("//button[@type='submit']");
     public static final By PASSWORD_INPUT = By.xpath("//input[@id='password']");
     public static final By LOG_IN_INPUT = By.xpath("//input[@id='login']");
     public static final By ERROR_MESSAGE = By.xpath("//div[@class='alert alert-danger' and @ng-show='formStatus']");
 
+    public static final By Password_REMINDER = By.xpath("//a[@href=\"/account/password_reminder\"]");
 
+    public static final By REGISTER = By.xpath("//a[@href='/account/registration' and normalize-space()='Register']");
 
 
     public LoginPage(WebDriver driver) {
         super(driver);
 
-        public LoginPage open(){
-            driver.get(BASE_URL+"login");
+        public LoginPage open() {
+            driver.get(BASE_URL + "login");
             return this;
 
             public LoginPage inputLoginAndPassword
@@ -27,14 +29,15 @@ public class LoginPage extends BasePage {
             driver.findElement(PASSWORD_INPUT).sendKeys(password);
             return this;
 
+            public LoginPage clickLoginButton () {
+                driver.findElement(LOG_IN_BUTTON).click();
+                return new LoginPage(driver);
+
+                public String getErrorMessage () {
+                    return driver.findElement(ERROR_MESSAGE).getText();
+                }
 
 
+            }
 
-
-
-
-
-
-
-
-
+        }
