25bc088f3d74060e551e0959d63c6e1eb96b46a9
Mikhailqwqwqwqwqwqwqwqwqwqwqwqwq <mishavitebsk96@gmail.com>
2023-08-15 14:43:51 +0300

    New Update

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 public class HomePage extends BasePage {
 
 public static final By LOG_IN_BUTTON = By.xpath("//a[@class='btn btn-primary'][text()='Login']");
-public static final By REGISTER_BUTTON = By.xpath("//a[@class='btn btn-primary home__register-btn']");
+public static final By SIGN_UP_BUTTON = By.xpath("//a[@class='btn btn-primary home__register-btn']");
 public static final By DONATION = By.xpath("//a[contains(text(), 'Donate now')]");
 
 
