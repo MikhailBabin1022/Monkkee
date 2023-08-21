a10942393a58998fcef3dde0d5856db74e736314
Mikhailqwqwqwqwqwqwqwqwqwqwqwqwq <mishavitebsk96@gmail.com>
2023-08-21 22:56:24 +0300

    New Update

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,17 +1,12 @@
 package tests;
 
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.MainPage;
-
-import javax.management.Descriptor;
-
-import static org.testng.AssertJUnit.assertTrue;
 
 public class LoginTest extends BaseTest {
 
-        @Test(description = "User validate login and password")
+
+        @Test(description = "User validates login and password")
         public void inputValidDateUserAndPassword() {
                 loginPage.open();
                 loginPage.inputLoginAndPassword("mislavik@tut.by", "8*!pDzuTH#75gYq");
@@ -20,11 +15,43 @@ public class LoginTest extends BaseTest {
                 Assert.assertTrue(loginPage.isPageOpen());
         }
 
+        @Test(description = "User enters invalid data")
+        public void inputInvalidDateUserAndPassword() {
+                loginPage.open();
+                loginPage.inputLoginAndPassword("wewewew", "wewewewew");
+                loginPage.clickLoginButton();
+                loginPage.isPageOpen();
+                Assert.assertTrue(loginPage.getErrorMessage().equals("Login_failed"));
+
         }
 
+        @Test(description = "User did not enter a password")
+        public void inputNotPassword() {
+                loginPage.open();
+                loginPage.inputLoginAndPassword("mislavik@tut.by", "");
+                loginPage.clickLoginButton();
+                loginPage.isPageOpen();
+                Assert.assertTrue(loginPage.getErrorMessage().equals("Mandatory field"));
+        }
 
+        @Test(description = "The user has not entered a login")
+        public void inputNotEnteredLogin() {
+                loginPage.open();
+                loginPage.inputLoginAndPassword("", "8*!pDzuTH#75gYq");
+                loginPage.clickLoginButton();
+                loginPage.isPageOpen();
+                Assert.assertTrue(loginPage.getErrorMessage().equals("Mandatory field"));
+        }
 
-
+        @Test(description = "The user did not enter a user and password")
+        public void inputNotUserAndPassword() {
+                loginPage.open();
+                loginPage.inputLoginAndPassword("", "");
+                loginPage.clickLoginButton();
+                loginPage.isPageOpen();
+                Assert.assertTrue(loginPage.getErrorMessage().equals("Mandatory field"));
+        }
+}
 
 
 
