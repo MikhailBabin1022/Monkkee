0474bc614badb88e4cb114a51433d763cd56de33
Mikhailqwqwqwqwqwqwqwqwqwqwqwqwq <mishavitebsk96@gmail.com>
2023-08-21 16:17:50 +0300

    New Update

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,11 +5,13 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.MainPage;
 
+import javax.management.Descriptor;
+
 import static org.testng.AssertJUnit.assertTrue;
 
 public class LoginTest extends BaseTest {
 
-        @Test(description = "User login and password valid data")
+        @Test(description = "User validate login and password")
         public void inputValidDateUserAndPassword() {
                 loginPage.open();
                 loginPage.inputLoginAndPassword("mislavik@tut.by", "8*!pDzuTH#75gYq");
