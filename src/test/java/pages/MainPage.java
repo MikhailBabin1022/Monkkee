70ea1c230e46686508c5d56815aafae4ed4f4d40
Mikhailqwqwqwqwqwqwqwqwqwqwqwqwq <mishavitebsk96@gmail.com>
2023-08-31 16:15:33 +0300

    Fixed Main Page

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,65 +1,99 @@
 package pages;
 
+import io.qameta.allure.Step;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import lombok.extern.log4j.Log4j2;
 import org.slf4j.Logger;
 
+import java.net.URL;
+
+@Log4j2
 public class MainPage extends BasePage {
 
     public static final By SETTINGS_BUTTON = By.xpath("//a[@class='user-menu__btn' and @href='#/settings/locale' and contains(., 'Settings')]");
     public static final By LOGOUT_BUTTON = By.xpath("//button[@class='user-menu__btn' and @ng-click='logout($event)']");
     public static final By CREATE_AN_ENTRY = By.xpath("//i[@class='icon-plus']");
     public static final By DELETE_ENTRY = By.id("delete-entry");
-    public static final By PRINT = By.xpath("//a[@class='btn btn-default' and @href='javascript:void(0)' and @title='Print entry']");
+    public static final By PRINT_BUTTON = By.xpath("//a[@class='btn btn-default' and @href='javascript:void(0)' and @title='Print entry']");
     public static final By ICON_LEFT = By.xpath("//i[@class='icon-chevron-left']");
     public static final By ICON_RIGHT = By.xpath("//i[@class='icon-chevron-right']");
     public static final By HOME_BUTTON = By.xpath("//a[@class=\"btn btn-default\" and @href=\"#/entries\" and @id=\"back-to-overview\" and @title=\"Back to overview\"]");
     public static final By SAVE_BUTTON = By.xpath("//a[@class='cke_button cke_button__savetoggle cke_button_off cke_button_disabled']");
+    public static final By TEXT_INPUT_FIELD = By.xpath("//div[contains(@class,'contenteditable cke_editable')]");
     private Logger log;
 
     public MainPage(WebDriver driver) {
         super(driver);
     }
 
-    public void createNewEntry(String text) {
+    @Step("Open page")
+    public MainPage open() {
+        driver.get(URL + "app/#/entries");
+        log.info("Open page with URL:" + URL);
+        return this;
+    }
+
+    @Step("Create a new entry with the text")
+    public MainPage createNewEntryWithText(String text){
         driver.findElement(CREATE_AN_ENTRY).click();
-        log.info("Create new entry");
-    }
-
-    public void deleteEntry() {
-        driver.findElement(DELETE_ENTRY).click();
-        log.info("Deleting the entry");
-
-    }
-
-    public void printEntry() {
-        driver.findElement(PRINT).click();
-        log.info("Printing the entry");
-    }
-
-    public void clickForwardButton() {
-        driver.findElement(ICON_RIGHT).click();
-        log.info("Clicking the forward button");
-    }
-
-    public void clickBackwardButton() {
-        driver.findElement(ICON_LEFT).click();
-        log.info("Clicking the backward button");
-    }
-
-    public void clickHomeButton() {
-        driver.findElement(HOME_BUTTON).click();
-        log.info("Clicking the home button");
-    }
-
-    public void clickSaveButton() {
+        log.info("Push button CREATE_AN_ENTRY");
+        driver.findElement(TEXT_INPUT_FIELD).sendKeys(text);
+        log.info("Entry field opened");
         driver.findElement(SAVE_BUTTON).click();
-        log.info("Clicking the save button");
+        log.info("Press save button");
+        return this;
     }
 
-    @Override
-    public boolean isPageOpen() {
-        return isExist(SETTINGS_BUTTON);
+    @Step("Delete entry")
+    public MainPage deleteEntry(){
+        driver.findElement(DELETE_ENTRY).click();
+        log.info("Press delete with Xpath");
+        Alert alert= driver.switchTo().alert();
+        alert.accept();
+        return this;
+    }
+
+    @Step("Click the Home Button")
+    public MainPage clickHomeButton(){
+        driver.findElement(HOME_BUTTON).click();
+        log.info("Press home button+Xpath");
+        return this;
+    }
+
+    @Step("Logout recording")
+    public MainPage logoutRecording() {
+        driver.findElement(LOGOUT_BUTTON).click();
+        log.info("Press logout button with Xpath");
+        return this;
+    }
+
+    @Step("Click Print Button")
+    public MainPage clickPrintbutton() {
+        driver.findElement(PRINT_BUTTON).click();
+        log.info("Press print button+Xpath");
+        return this;
+    }
+
+    @Step("Click to the Settings Button")
+    public MainPage clickSettingsButton(){
+        driver.findElement(SETTINGS_BUTTON).click();
+        log.info("Press Settings button +Xpath");
+        return this;
+    }
+
+    @Step("Click Icon Left")
+    public MainPage clickIconLeft() {
+        driver.findElement(ICON_LEFT).click();
+        log.info("Press Icon left +Xpath");
+        return this;
+    }
+
+    @Step("Click Icon Right")
+    public MainPage clickIconRight() {
+        driver.findElement(ICON_RIGHT).click();
+        log.info("Press Icon left +Xpath");
+        return this;
     }
 }
