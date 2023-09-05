09fd04b515eee2ea5d3416e4dabd509bffddce57
Mikhailqwqwqwqwqwqwqwqwqwqwqwqwq <mishavitebsk96@gmail.com>
2023-09-05 16:20:13 +0300

    Fix LoginTest

--- src/test/java/tests/ImageUploadTest.java
+++ src/test/java/tests/ImageUploadTest.java
@@ -1,8 +1,21 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.ImageUploadPage;
+import pages.MainPage;
+
+import java.time.Duration;
+
 public class ImageUploadTest extends BaseTest {
 
-
-
-
+    public ImageUploadTest() {
+    }
 }
+
