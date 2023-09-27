1f1eedc54aa9ccd636167f1212c845d73e740b6a
Mikhailqwqwqwqwqwqwqwqwqwqwqwqwq <mishavitebsk96@gmail.com>
2023-09-27 14:10:21 +0300

    Fixed ImageUploadTest and ImageUploadPage

--- src/test/java/pages/ImageUploadPage.java
+++ src/test/java/pages/ImageUploadPage.java
@@ -1,10 +1,12 @@
 package pages;
 
+import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import java.io.File;
-
+@Log4j2
 public class ImageUploadPage extends BasePage {
 
     public static final By IMAGE_DOWNLOAD_BUTTON = By.xpath("//span[@id='cke_267_description']");
@@ -14,9 +16,9 @@ public class ImageUploadPage extends BasePage {
     public static final By LEFT_RADIO_BUTTON = By.xpath("//input[@type='radio' and @class='cke_dialog_ui_radio_input' and @name='align_radio' and @value='left' and @id='cke_394_uiElement']");
     public static final By CENTER_RADIO_BUTTON = By.xpath("//input[@type='radio' and @class='cke_dialog_ui_radio_input' and @name='align_radio' and @value='center' and @id='cke_396_uiElement']");
     public static final By RIGHT_RADIO_BUTTON = By.xpath("//input[@type='radio' and @class='cke_dialog_ui_radio_input' and @name='align_radio' and @value='right' and @id='cke_398_uiElement']");
-    public static final By OK_BUTTON = By.xpath("//span[@id='cke_405_label' and @class='cke_dialog_ui_button' and text()='OK']");
+    public static final By OK_BUTTON_UPLOAD = By.xpath("//span[@id='cke_405_label' and @class='cke_dialog_ui_button' and text()='OK']");
     public static final By CANCEL_BUTTON = By.xpath("//span[@id='cke_407_label' and @class='cke_dialog_ui_button' and text()='Cancel']");
-
+    public static final By IMAGE_PRESENT = By.xpath("//span[@data-cke-widget-wrapper='1']");
     public ImageUploadPage(WebDriver driver) {
         super(driver);
     }
@@ -34,9 +36,27 @@ public class ImageUploadPage extends BasePage {
     public ImageUploadPage uploadFile() {
         File file = new File("src/test/resources/Depeche-Mode-wallpaper-3ee848ff8d502a9856541997f1095905.jpg");
         driver.findElement(UPLOAD_BUTTON).sendKeys(file.getAbsolutePath());
-        driver.findElement(OK_BUTTON).click();
+        driver.findElement(OK_BUTTON_UPLOAD).click();
         return this;
     }
 
+    public ImageUploadPage clickOkButton() {
+        driver.findElement(OK_BUTTON_UPLOAD).click();
+        log.info("click+OK Button");
+        return this;
+    }
+    @Step("Image Present on Page")
+    public boolean imagePresent() {
+        return driver.findElement(IMAGE_PRESENT).isDisplayed();
+    }
+
+
+
+
+
+
+
 
 }
+
+
--- src/test/java/tests/ImageUploadTest.java
+++ src/test/java/tests/ImageUploadTest.java
@@ -1,25 +1,27 @@
 package tests;
 
 import org.openqa.selenium.By;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.ImageUploadPage;
 
 import java.io.File;
 
 import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class ImageUploadTest extends BaseTest {
 
-    @Test
-    public void FileUpload() {
-        driver.get("https://monkkee.com/app/#/entries/3784723");
+    @Test(description = "Upload Image")
+    public void ImageUpload() throws InterruptedException {
+        loginPage.open()
+                .inputLoginAndPassword("hsaat@mailto.plus","BC12345WGHHGHGHGHGHVG")
+                        .clickLoginButton()
+                                .createNewEntryWithText("");
+        imageUploadPage.clickUploadButton()
+                .uploadFile()
+                .clickOkButton();
+        assertTrue(imageUploadPage.imagePresent());
 
-        File file = new File("src/test/resources/Depeche-Mode-wallpaper-3ee848ff8d502a9856541997f1095905.jpg");
-        driver.findElement(By.xpath("\"//input[@id='cke_388_fileInput_input' and @type='file' and @name='txtUpload']\"")).sendKeys(file.getAbsolutePath());
-        driver.findElement(By.xpath("//span[@id='cke_405_label' and @class='cke_dialog_ui_button' and text()='OK']")).click();
 
-       assertEquals( "File Uploaded!","File  Uploaded");
     }
 }
 
